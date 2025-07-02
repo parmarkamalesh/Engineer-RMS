b8b554dcee386b8a29fc12137110335491c35bba
parmarkamalesh <kamalesh.py@gmail.com>
2025-07-02 16:33:24 +0530

    Update
    Update

--- server/models/Project.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const projectSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  deadline: Date,
-});
-
-export default mongoose.model("Project", projectSchema);
